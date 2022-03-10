450f25c90f5f90e24b5e350fc9571a6579d304fb
andreafodarella <92960465+andreafodarella@users.noreply.github.com>
2022-03-10 15:29:36 +0100

    Unity, day one

---
+++ GAME DEV, create with code/Prototipo 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //to do, vehicle movement
+       transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+    }
+}
